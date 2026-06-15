b1eed3a220449aafb3d079f0e2d0734d78b116cf
Abdulrhman-alatawi <date.tool.official@gmail.com>
2026-06-15 22:59:15 +0300

    Fix Next.js build

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 🌟 هذا هو السطر السحري الذي سيقوم بإنشاء مجلد out
-  trailingSlash: true,
   images: {
     unoptimized: true,
-  }
+  },
 };
 
 export default nextConfig;
