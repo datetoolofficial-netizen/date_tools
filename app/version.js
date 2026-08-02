31c96fa246d688359676b68f159e164dee516f58
Abdulrhman-alatawi <date.tool.official@gmail.com>
2026-08-02 18:03:08 +0300

    Polish admin tool management layout

--- app/version.js
+++ app/version.js
@@ -1,4 +1,4 @@
 export const APP_VERSION = '0.3.9';
 export const APP_VERSION_DATE = '2026-08-02';
-export const ADMIN_VERSION = '0.1.8';
+export const ADMIN_VERSION = '0.1.9';
 export const ADMIN_VERSION_DATE = '2026-08-02';
