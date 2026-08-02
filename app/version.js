874ede2c73cbbf59b710acc89cee1b92208d1d7f
Abdulrhman-alatawi <date.tool.official@gmail.com>
2026-08-02 21:23:58 +0300

    Unify admin component styles

--- app/version.js
+++ app/version.js
@@ -1,4 +1,4 @@
 export const APP_VERSION = '0.3.9';
 export const APP_VERSION_DATE = '2026-08-02';
-export const ADMIN_VERSION = '0.1.11';
+export const ADMIN_VERSION = '0.1.12';
 export const ADMIN_VERSION_DATE = '2026-08-02';
