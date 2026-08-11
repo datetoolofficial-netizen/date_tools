cfaaf1f545e41c3fdb4912129a7457512ea7dce0
Abdulrhman-alatawi <date.tool.official@gmail.com>
2026-08-11 12:12:55 +0300

    fix: balance contact modal margins

--- app/version.js
+++ app/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '0.3.24';
+export const APP_VERSION = '0.3.25';
 export const APP_VERSION_DATE = '2026-08-11';
 export const ADMIN_VERSION = '0.1.28';
 export const ADMIN_VERSION_DATE = '2026-08-10';
