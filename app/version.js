66f943f64aea086972ca8589786a11ae0c995716
Abdulrhman-alatawi <datetoolofficial-netizen@users.noreply.github.com>
2026-09-10 13:15:22 +0300

    security: patch runtime and test dependencies

--- app/version.js
+++ app/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '0.3.55';
+export const APP_VERSION = '0.3.56';
 export const APP_VERSION_DATE = '2026-09-10';
 export const ADMIN_VERSION = '0.1.53';
 export const ADMIN_VERSION_DATE = '2026-09-10';
